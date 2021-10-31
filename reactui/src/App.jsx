48ed111f6fb64fc60738ddacf8263a160d73e631
suyashsonawane <suyashsonawane007@gmail.com>
2021-10-31 20:02:33 +0530

    react started

--- reactui/src/App.jsx
+++ reactui/src/App.jsx
@@ -1,56 +1,71 @@
-import './App.css';
-
+import React, { useRef, useState } from 'react'
 import Peer from "peerjs"
-import { useEffect, useRef, useState } from 'react';
+import "./App.css"
 
-function App() {
+/*
+
+0 - screen sharing starts
+1 - screen sharing stops
+2 - video status
+3 - audio status
+
+DATA FORMAT
+{
+  mode:{ALERT | MOUSE-DATA | MOUSE-CLICK-RIGHT | MOUSE-CLICK-LEFT | KEY | SCROLL},
+  data:{INT | STRING}
+}
+
+*/
+
+export default function App() {
+
   const PRE = "SIMPLE"
   const SUF = "CONTROLLER"
   let room_id;
   let getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
   let local_stream;
   let screenStream;
+  let peer = null;
+  let currentPeer = null
   let screenSharing = false
   let remoteConnection = null
+  let socket = null
   let dataString = null
   let lastDataString = null
   let sendInterval = null
 
-
-  const [showStartStreamingBtn, setShowStartStreamingBtn] = useState(false)
-  const [showStartControllingBtn, setShowControllingBtn] = useState(false)
+  const [showStartScreenShareBtn, setShowStartScreenShareBtn] = useState(false)
+  const [showStopScreenShareBtn, setShowStopScreenShareBtn] = useState(false)
+  const [showCursorToggleBtn, setShowCursorToggleBtn] = useState(false)
+  const [isAudio, setIsAudio] = useState(true)
+  const [isCursorHidden, setIsCursorHidden] = useState(false)
+  const [isVideo, setIsVideo] = useState(true)
+  const [showMeetBar, setShowMeetBar] = useState(false)
   const [roomID, setRoomID] = useState("")
-  const [error, setError] = useState("")
-  const [socket, setSocket] = useState(null)
-  const remoteVideoElement = useRef(null)
-  const localVideoElement = useRef(null)
-  const [peer, setPeer] = useState(null)
-  const [currentPeer, setCurrentPeer] = useState(null)
+  const [title, setTitle] = useState("Simple Controller")
 
-
-  useEffect(() => {
-    console.log(peer)
-  }, [peer])
+  const remoteVideo = useRef(null)
+  const localVideo = useRef(null)
 
   function createRoom() {
     console.log("Creating Room")
-    let room = roomID
+    let room = roomID;
     if (room == " " || room == "") {
-      setError("Please enter room number")
+      alert("Please enter room number")
       return;
     }
     room_id = PRE + room + SUF;
-    let peer = new Peer(room_id)
-    setPeer(peer)
+    peer = new Peer(room_id)
     peer.on('open', (id) => {
       console.log("Peer Connected with ID: ", id)
+      hideModal()
       getUserMedia({ video: true, audio: true }, (stream) => {
         local_stream = stream;
         setLocalStream(local_stream)
       }, (err) => {
         console.log(err)
       })
-      setError("Waiting for peer to join.")
+      notify("Waiting for peer to join.")
     })
     peer.on('connection', conn => {
       remoteConnection = conn
@@ -63,7 +78,8 @@ function App() {
       call.on('stream', (stream) => {
         setRemoteStream(stream)
       })
-      setCurrentPeer(call)
+      currentPeer = call;
+      setShowMeetBar(true)
     })
   }
 
@@ -107,13 +123,13 @@ function App() {
   }
 
   function attachHandlers() {
-    local_stream.getTracks().forEach(function (track) {
-      track.stop();
-    });
-    remoteVideoElement.current.oncontextmenu = (e) => {
+    setShowStartScreenShareBtn(false)
+    setShowCursorToggleBtn(true)
+    document.getElementById("remote-video").oncontextmenu = (e) => {
       let offset = document.querySelector('#remote-video').getBoundingClientRect();
       let pos = { x: e.pageX - offset.left, y: e.pageY - offset.top }
       if (pos.x >= 0 && pos.x < offset.width && pos.y >= 0 && pos.y < offset.height) {
+        // if (Math.random() > 0.5)
         remoteConnection.send(JSON.stringify({
           mode: "MOUSE-CLICK-RIGHT",
           data: `${Math.round(offset.width)}-${Math.round(offset.height)}-${Math.round(pos.x)}-${Math.round(pos.y)}`
@@ -123,7 +139,7 @@ function App() {
     }
     document.body.addEventListener("wheel", handleScrollWheel)
     document.body.addEventListener("keydown", handleKeyPress)
-    remoteVideoElement.current.addEventListener("click", handleMouseClick)
+    document.getElementById("remote-video").addEventListener("click", handleMouseClick)
     document.addEventListener('mousemove', handleMouseMove)
     sendInterval = setInterval(() => {
       if (dataString !== lastDataString)
@@ -132,27 +148,17 @@ function App() {
           data: dataString
         }))
       lastDataString = dataString
-    }, 100);
+    }, 60);
   }
   function removeHandlers() {
-    getUserMedia({ video: true, audio: true }, (stream) => {
-      local_stream = stream;
-      setLocalStream(local_stream)
-      let videoTrack = local_stream.getVideoTracks()[0];
-      if (peer) {
-        let sender = currentPeer.peerConnection.getSenders().find(function (s) {
-          return s.track.kind == videoTrack.kind;
-        })
-        sender.replaceTrack(videoTrack)
-      }
-
-    })
+    setShowStartScreenShareBtn(true)
+    setShowCursorToggleBtn(false)
     clearInterval(sendInterval)
     document.body.removeEventListener("keydown", handleKeyPress)
     document.body.removeEventListener("wheel", handleScrollWheel)
-    remoteVideoElement.current.removeEventListener("click", handleMouseClick)
+    document.getElementById("remote-video").removeEventListener("click", handleMouseClick)
     document.removeEventListener('mousemove', handleMouseMove)
-    remoteVideoElement.current.oncontextmenu = null
+    document.getElementById("remote-video").oncontextmenu = null
   }
 
   function handleRemoteData(data) {
@@ -161,13 +167,16 @@ function App() {
       if (data['data'] === 0) {
         console.log("staring screen share")
         attachHandlers()
-
       }
       else
         if (data['data'] === 1) {
           console.log("stoping screen share")
           removeHandlers()
         }
+        else if (data['data'] === 2)
+          notify(`remote video enabled: ${data['status']}`)
+        else if (data['data'] === 3)
+          notify(`remote audio enabled: ${data['status']}`)
     }
     else
       if (data['mode'] === 'MOUSE-DATA') {
@@ -200,36 +209,70 @@ function App() {
 
   }
 
-  function setLocalStream(stream) {
+  function toggleVideo() {
+    setIsVideo(!isVideo)
+    local_stream.getVideoTracks()[0].enabled = isVideo
+    remoteConnection.send(JSON.stringify({
+      mode: "ALERT",
+      data: 2,
+      status: isVideo
+    }))
+  }
+  function toggleAudio() {
+    setIsAudio(!isAudio)
+    local_stream.getAudioTracks()[0].enabled = isAudio
+    remoteConnection.send(JSON.stringify({
+      mode: "ALERT",
+      data: 3,
+      status: isAudio
+    }))
+  }
+  function cursorToggle() {
+    remoteVideo.current.style.cursor = isCursorHidden ? "default" : "none"
+    setIsCursorHidden(!isCursorHidden)
+  }
 
+  function setLocalStream(stream) {
     let video = document.getElementById("local-video");
     video.srcObject = stream;
     video.muted = true;
     video.play();
   }
   function setRemoteStream(stream) {
-    let video = remoteVideoElement.current;
+    let video = document.getElementById("remote-video");
     video.srcObject = stream;
     video.play();
   }
 
+  function hideModal() {
+    document.getElementById("entry-modal").hidden = true
+  }
+
+  function notify(msg) {
+    let notification = document.getElementById("notification")
+    notification.innerHTML = msg
+    notification.hidden = false
+    setTimeout(() => {
+      notification.hidden = true;
+    }, 5000)
+  }
 
   function joinRoom() {
     console.log("Joining Room")
-    let room = roomID
+    let room = roomID;
     if (room == " " || room == "") {
-      setError("Please enter room number")
+      alert("Please enter room number")
       return;
     }
     room_id = PRE + room + SUF;
-    let peer = new Peer()
-    setPeer(peer)
+    hideModal()
+    peer = new Peer()
     peer.on('open', (id) => {
       console.log("Connected with Id: " + id)
       getUserMedia({ video: true, audio: true }, (stream) => {
         local_stream = stream;
         setLocalStream(local_stream)
-        setError("Joining peer")
+        notify("Joining peer")
         let call = peer.call(room_id, stream)
         let connection = peer.connect(room_id)
 
@@ -241,10 +284,10 @@ function App() {
         })
 
         call.on('stream', (stream) => {
-          console.log(stream)
           setRemoteStream(stream);
         })
-        setCurrentPeer(call)
+        currentPeer = call;
+        setShowMeetBar(true)
       }, (err) => {
         console.log(err)
       })
@@ -256,35 +299,40 @@ function App() {
     if (screenSharing) {
       stopScreenSharing()
     }
-    local_stream && local_stream.getTracks().forEach(function (track) {
-      track.stop();
-    });
-    navigator.mediaDevices.getDisplayMedia({ video: true }).then((stream) => {
-      screenStream = stream;
-      let videoTrack = screenStream.getVideoTracks()[0];
-      videoTrack.onended = () => {
-        stopScreenSharing()
-      }
-      if (peer) {
-        let sender = currentPeer.peerConnection.getSenders().find(function (s) {
-          return s.track.kind == videoTrack.kind;
+    try {
+      socket = new WebSocket('ws://127.0.0.1:5678');
+      // $("#remote-video").hide()
+      socket.addEventListener('open', function (event) {
+        navigator.mediaDevices.getDisplayMedia({ video: true }).then((stream) => {
+          screenStream = stream;
+          let videoTrack = screenStream.getVideoTracks()[0];
+          videoTrack.onended = () => {
+            stopScreenSharing()
+          }
+          if (peer) {
+            let sender = currentPeer.peerConnection.getSenders().find(function (s) {
+              return s.track.kind == videoTrack.kind;
+            })
+            sender.replaceTrack(videoTrack)
+            screenSharing = true
+            remoteConnection.send(JSON.stringify({
+              mode: "ALERT",
+              data: 0
+            }))
+          }
+          setTitle("Screen is being shared")
+          console.log(screenStream)
+          setShowStartScreenShareBtn(false)
+          setShowStopScreenShareBtn(true)
         })
-        sender.replaceTrack(videoTrack)
-        screenSharing = true
-        let socket = new WebSocket('ws://127.0.0.1:5678')
-        setSocket(socket)
-        remoteVideoElement.current.style.display = "none"
-        document.getElementById("title").innerHTML = "Screen is being shared"
-        socket.addEventListener('open', function (event) {
-          console.log("connected with local socket")
-        });
-        remoteConnection.send(JSON.stringify({
-          mode: "ALERT",
-          data: 0
-        }))
+        console.log("connected with local socket")
+      });
+      socket.onerror = (event) => {
+        notify("Cannot connect to client, ensure client is running")
       }
-      console.log(screenStream)
-    })
+    } catch (e) {
+      notify(e.message)
+    }
   }
 
   function stopScreenSharing() {
@@ -299,67 +347,47 @@ function App() {
     screenStream.getTracks().forEach(function (track) {
       track.stop();
     });
-    remoteVideoElement.current.style.display = "block"
-    document.getElementById("title").innerHTML = "Simple Controller"
+    // $("#remote-video").show()
+    setTitle("Simple Controller")
     socket.close()
     remoteConnection.send(JSON.stringify({
       mode: "ALERT",
       data: 1
     }))
-    getUserMedia({ video: true, audio: true }, (stream) => {
-      local_stream = stream;
-      setLocalStream(local_stream)
-      let videoTrack = local_stream.getVideoTracks()[0];
-      if (peer) {
-        let sender = currentPeer.peerConnection.getSenders().find(function (s) {
-          return s.track.kind == videoTrack.kind;
-        })
-        sender.replaceTrack(videoTrack)
-      }
-
-    })
+    setShowStartScreenShareBtn(true)
+    setShowStopScreenShareBtn(false)
     screenSharing = false
   }
+
   return (
-    <>
-      <div className="header">
-        <h1 id="title">Simple Controller</h1>
-        <h5>Remote Peer Connected</h5>
-        <h5>Local Socket Connected</h5>
-      </div>
-      {!peer && <div className="connection-dialogue">
-        Enter Connection ID: <input type="text" value={roomID} onChange={e => setRoomID(e.target.value)} />
-        <div className="options">
-          <div className="btn" onClick={createRoom}>Create Room</div>
-          <div className="btn" onClick={joinRoom}>Join Room</div>
-        </div>
-      </div>}
-      <video id="remote-video" ref={remoteVideoElement}></video>
-      <div className="floating-div">
-        {peer && <p onClick={startScreenShare}>Start Streaming</p>}
-        {peer && <p>Start Controlling</p>}
-      </div>
-      {error && <div className="error">
-        {error}
-      </div>}
-      {/* <p id="notification" hidden></p>
-      <div class="entry-modal" id="entry-modal">
-        <p>Create or Join Meeting</p>
-        <input id="room-input" class="room-input" placeholder="Enter Room ID" />
+    <div>
+      <h1 className="title" id="title">{title}</h1>
+      <p id="notification" hidden></p>
+      <div className="entry-modal" id="entry-modal">
+        <h2>Create or Join Meeting</h2>
+        <input id="room-input" className="room-input" placeholder="Enter Room ID" value={roomID} onChange={e => { setRoomID(e.target.value) }} />
         <div>
           <button onClick={createRoom}>Create Room</button>
           <button onClick={joinRoom}>Join Room</button>
         </div>
       </div>
-      <div class="meet-area">
-      
-      <div class="meet-controls-bar">
-      <button onClick={startScreenShare}>Screen Share</button>
+      <div className="meet-area">
+        <video id="remote-video" ref={remoteVideo}></video>
+        <video id="local-video" ref={localVideo}></video>
+        {showMeetBar &&
+          <div className="meet-controls-bar">
+            <button id="videoToggle" onClick={toggleVideo} className={`${isVideo ? "active" : ""}`}>Video</button>
+            <button id="audioToggle" onClick={toggleAudio} className={`${isAudio ? "active" : ""}`}>Audio</button>
+            {showStartScreenShareBtn && <button id="startScreenShareBtn" onClick={startScreenShare}>Screen Share</button>}
+            {showStopScreenShareBtn && <button id="stopScreenShareBtn" onClick={stopScreenSharing}>Stop Share</button>}
+            {showCursorToggleBtn && <button onClick={cursorToggle} className={`${isCursorHidden ? "active" : ""}`} >Hide Cursor</button>}
+          </div>
+        }
       </div>
-    </div> */}
-      <video id="local-video" ref={localVideoElement}></video>
-    </>
-  );
+      <div id="downloadClient">
+        <img src="images/download.png" />
+        Download Client
+      </div>
+    </div>
+  )
 }
-
-export default App;
